6c9ef7c781baac2420e8f362662e2348222cecd5
Rachel <80181684+rappleit@users.noreply.github.com>
2023-11-24 22:32:43 +0800

    added model

---
+++ README.md
@@ -0,0 +1,22 @@
+# From Policy to Plate
+
+## Overview
+
+A web-based prediction tool to predict the level of food insecurity in a given country based on the quality of its governance. Created for SUTD's 2023 Term 3 2D Project
+
+## Installation
+
+To install and run the app, follow these steps:
+
+1. Clone the repository:
+
+    ```bash
+    git clone https://github.com/yourusername/my-flask-app.git
+    cd my-flask-app
+    ```
+
+2. Install dependencies:
+
+    ```bash
+    pip install -r requirements.txt
+    ```
--- project/app/routes.py
+++ project/app/routes.py
@@ -1,10 +1,13 @@
 from app import application
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from werkzeug.urls import url_parse
 from app import db
 from flask import request 
 from app.serverlibrary import * 
+import pickle
+import numpy as np
 
+model = pickle.load((open("predMod.pkl", "rb")))
 
 @application.route('/')
 @application.route('/index')
@@ -12,6 +15,17 @@ def index():
 	return render_template('index.html', title='Predictor') 
 
 # write down your handler for the routes here
+@application.route("/predict", methods=['POST'])
+def prediction():
+	coc = request.form["corruptionValue"]
+	pol = request.form["polValue"]
+	rol = request.form["rolValue"]
+	ge = request.form["geValue"]
+	lr = request.form["lrValue"]
+	inputArr = [coc, pol, rol, ge, lr]
+	arr = np.array([[0, coc, pol, rol, ge, lr]])
+	pred = model.predict(arr)
+	return render_template('index.html', title='Predictor', data=pred, input=inputArr) 
 
 
 
